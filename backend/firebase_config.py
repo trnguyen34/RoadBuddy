b3dca497e9ea64ef8779c4ee5e5e358be9ecd032
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2025-01-31 08:20:18 -0800

    merge main into frontend for pull req prep

---
+++ backend/firebase_config.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyBd4XkPoarNwcdyevtpyKRJfkskyU0duT4",
+  "authDomain": "roadbuddy-4edc7.firebaseapp.com",
+  "projectId": "roadbuddy-4edc7",
+  "storageBucket": "roadbuddy-4edc7.firebasestorage.app",
+  "messagingSenderId": "217816938704",
+  "appId": "1:217816938704:web:f8632ee3ed001716c886d5",
+  'measurementId': "G-KTRN6PKGT9"
+}
