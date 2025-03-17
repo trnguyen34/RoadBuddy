dd91bb78cfd9af46ec5b546fcdb76d601895b3c1
Truong <trnguyen34@gmail.com>
2025-03-17 13:24:36 -0700

    add add add_joined_ride()

--- backend/RoadBuddy/src/services/user_manager.py
+++ backend/RoadBuddy/src/services/user_manager.py
@@ -25,7 +25,7 @@ class UserManager:
 
     def add_posted_ride(self, ride_id):
         """
-        Adds a ride to the user's "ridesPosted" list in Firestore.
+        Add a ride to the user's "ridesPosted" list in Firestore.
         """
         try:
             user_doc = self.user_ref.get()
@@ -36,10 +36,44 @@ class UserManager:
             rides_posted = user_data.get('ridesPosted', [])
 
             if ride_id in rides_posted:
-                return {"message": "User has already joined this ride"}, 200
+                return {"message": "User has already posted this ride"}, 200
 
             rides_posted.append(ride_id)
             self.user_ref.update({"ridesPosted": rides_posted})
+
+            return {
+                "message": "Ride successfully added to user's posted rides"
+            }, 200
+
+        except FirebaseError as e:
+            return {
+                "error": "Failed to add ride to user's posted rides",
+                "details": str(e)
+            }, 500
+
+        except Exception as e:
+            return {
+                "error": "An unexpected error occurred",
+                "details": str(e)
+            }, 500
+
+    def add_joined_ride(self, ride_id):
+        """
+        Add a ride to the iser's "rideJoined" list in Firestore.
+        """
+        try:
+            user_doc = self.user_ref.get()
+            if not user_doc.exists:
+                return {"error": "User not found"}, 404
+
+            user_data = user_doc.to_dict()
+            rides_joined = user_data.get('ridesJoined', [])
+
+            if ride_id in rides_joined:
+                return {"message": "User has already joined this ride"}, 200
+
+            rides_joined.append(ride_id)
+            self.user_ref.update({"ridesJoined": rides_joined})
 
             return {
                 "message": "Ride successfully added to user's joined rides"
@@ -56,4 +90,3 @@ class UserManager:
                 "error": "An unexpected error occurred",
                 "details": str(e)
             }, 500
-
