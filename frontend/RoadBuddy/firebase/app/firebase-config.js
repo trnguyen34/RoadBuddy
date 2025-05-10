391070cca8eafefb60b8292b1f2883aff7125d8b
trnguyen34 <52758200+trnguyen34@users.noreply.github.com>
2025-05-09 17:39:42 -0700

    Update firebase-config.js

--- frontend/RoadBuddy/firebase/app/firebase-config.js
+++ frontend/RoadBuddy/firebase/app/firebase-config.js
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBd4XkPoarNwcdyevtpyKRJfkskyU0duT4",
-  authDomain: "roadbuddy-4edc7.firebaseapp.com",
-  projectId: "roadbuddy-4edc7",
-  storageBucket: "roadbuddy-4edc7.firebasestorage.app",
-  messagingSenderId: "217816938704",
-  appId: "1:217816938704:web:f8632ee3ed001716c886d5",
-  measurementId: "G-KTRN6PKGT9"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
 
 // Initialize Firebase
